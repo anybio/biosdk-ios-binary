1dd4f39d318647f1472c039c633f9d48b6447b28
Stephen Saine <stephen@saine.com>
2025-11-17 09:51:15 -0800

    Update Package.swift to reference 1.0.2 release

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BioSDK",
-            url: "https://github.com/anybio/biosdk-ios-binary/releases/download/1.0.1/BioSDK.xcframework.zip",
+            url: "https://github.com/anybio/biosdk-ios-binary/releases/download/1.0.2/BioSDK.xcframework.zip",
             checksum: "c874982a73eea0abdfb66a4d4113e694ffc0f46d5219ef026a0fc1de86cffe24"
         ),
         .target(
