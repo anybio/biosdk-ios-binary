749aae7918f0185366b8a6b5783065b6c39f24b1
Stephen Saine <stephen@saine.com>
2025-11-17 21:54:25 -0800

    Release 1.0.4

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BioSDK",
-            url: "https://github.com/anybio/biosdk-ios-binary/releases/download/1.0.3/BioSDK.xcframework.zip",
-            checksum: "f72efb35493c1cf970294777e689f12812dc2732afc80b4a34110c770e4fed79"
+            url: "https://github.com/anybio/biosdk-ios-binary/releases/download/1.0.4/BioSDK.xcframework.zip",
+            checksum: "3a8e0acab70868ba06c1b8b21a2a0c92f57fd5e9c230dabf60ad28cff683d788"
         ),
         .target(
             name: "BioUI",
