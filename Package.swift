a3b8dfb95066964e3925409ae1d0cfa6d91bf4cf
Stephen Saine <stephen@saine.com>
2025-11-28 13:28:36 -0600

    Release 1.0.5

--- Package.swift
+++ Package.swift
@@ -14,15 +14,21 @@ let package = Package(
             targets: ["BioUI"]
         )
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.31.0")
+    ],
     targets: [
         .binaryTarget(
             name: "BioSDK",
-            url: "https://github.com/anybio/biosdk-ios-binary/releases/download/1.0.4/BioSDK.xcframework.zip",
-            checksum: "3a8e0acab70868ba06c1b8b21a2a0c92f57fd5e9c230dabf60ad28cff683d788"
+            url: "https://github.com/anybio/biosdk-ios-binary/releases/download/1.0.5/BioSDK.xcframework.zip",
+            checksum: "67946047937fed98916c2f042c0be44b7a7d8f0ea3c301f1b118dabeff302869"
         ),
         .target(
             name: "BioUI",
-            dependencies: ["BioSDK"],
+            dependencies: [
+                "BioSDK",
+                .product(name: "SwiftProtobuf", package: "swift-protobuf")
+            ],
             path: "Sources/BioUI"
         )
     ]
